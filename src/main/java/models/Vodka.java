3f62418f828de17a38b3dc66cdfb5b651022f8d4
Yun <yun@zipcoders-MacBook-Pro-2.local>
2022-03-28 12:51:27 -0400

    added properties to vodka

--- src/main/java/models/Vodka.java
+++ src/main/java/models/Vodka.java
@@ -1,4 +1,11 @@
 package models;
 
 public class Vodka {
+    private int id;
+    private String name;
+    private String brand;
+    private String region;
+    private int size;
+    private int qty;
+    private float price;
 }
