64037734e674e28866ffa14bc54d0891df03b906
Yun <yun@zipcoders-MacBook-Pro-2.local>
2022-03-30 10:28:12 -0400

    done part 2

--- src/main/java/services/VodkaService.java
+++ src/main/java/services/VodkaService.java
@@ -1,4 +1,60 @@
 package services;
 
+import models.Vodka;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.function.UnaryOperator;
+
 public class VodkaService {
+    private static int nextId = 1;  // (1)
+
+    private ArrayList<Vodka> inventory = new ArrayList<>();  // (2)
+
+    // (1)
+    public Vodka create(String name, String brand, String region, int size, int quantity, double price) {
+
+        // (2)
+        Vodka createdVodka = new Vodka(nextId++, name, brand, region, size, quantity, price);
+
+        // (3)
+        inventory.add(createdVodka);
+
+        // (4)
+        return createdVodka;
+    }
+    //read
+    public Vodka findVodka(int id) {
+        for(Vodka n : inventory){
+            if( n.getId() == id){
+                return n;
+            }
+
+        }
+        return null;
+        // should take an int and return an object with that id, if exists
+    }
+
+    //read all
+    public Vodka[] findAll() {
+        // should return a basic array copy of the ArrayList
+        Vodka[] list = new Vodka[inventory.size()];
+        for (int i= 0; i<list.length; i++){
+            list[i] = inventory.get(i);
+        }
+
+        return list;
+    }
+
+    //delete
+    public boolean delete(int id) {
+        if(inventory.contains(findVodka(id))){
+            inventory.remove(findVodka(id));
+            return true;
+        }else return false;
+//        // should remove the object with this id from the ArrayList if exits and return true.
+//        // Otherwise return false
+    }
 }
--- src/test/java/services/VodkaServiceTest.java
+++ src/test/java/services/VodkaServiceTest.java
@@ -1,7 +1,135 @@
 package services;
 
+import models.Vodka;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.security.Provider;
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class VodkaServiceTest {
+    @Test
+    public void createTest(){
 
+        // (1)
+        String expectedName = "Fire Water";
+        String expectedBrand = "FIRE";
+        String expectedRegion = "Philadelphia";
+        int expectedSize = 750;
+        int expectedQty = 10;
+        double expectedPrice = 80.00;
+
+        // (2)
+        VodkaService VodkaService = new VodkaService();
+        Vodka testVodka = VodkaService.create(expectedName, expectedBrand,
+                expectedRegion, expectedSize, expectedQty, expectedPrice);
+
+        // (3)
+        int actualId = testVodka.getId();
+        String actualName = testVodka.getName();
+        String actualBrand = testVodka.getBrand();
+        String actualRegion = testVodka.getRegion();
+        int actualSize = testVodka.getSize();
+        int actualQty = testVodka.getQty();
+        double actualPrice = testVodka.getPrice();
+
+        // (4)
+        Assertions.assertEquals(Integer.class.getName(), new Integer(actualId).getClass().getName());
+        Assertions.assertEquals(expectedName, actualName);
+        Assertions.assertEquals(expectedBrand, actualBrand);
+        Assertions.assertEquals(expectedRegion, actualRegion);
+        Assertions.assertEquals(expectedSize, actualSize);
+        Assertions.assertEquals(expectedQty, actualQty);
+        Assertions.assertEquals(expectedPrice, actualPrice);
+        VodkaService.delete(actualId);
+    }
+
+
+    @Test
+    void findVodka() {
+        // (1)
+        String expectedName = "Fire Water";
+        String expectedBrand = "FIRE";
+        String expectedRegion = "Philadelphia";
+        int expectedSize = 750;
+        int expectedQty = 10;
+        double expectedPrice = 80.00;
+
+        // (2)
+        VodkaService VodkaService = new VodkaService();
+        Vodka testVodka = VodkaService.create(expectedName, expectedBrand,
+                expectedRegion, expectedSize, expectedQty, expectedPrice);
+
+        // (3)
+        int actualId = testVodka.getId();
+        String actualName = testVodka.getName();
+        String actualBrand = testVodka.getBrand();
+        String actualRegion = testVodka.getRegion();
+        int actualSize = testVodka.getSize();
+        int actualQty = testVodka.getQty();
+        double actualPrice = testVodka.getPrice();
+        Vodka actual= VodkaService.findVodka(testVodka.getId());
+
+        Assertions.assertEquals(testVodka,actual);
+        VodkaService.delete(actualId);
+
+    }
+
+    @Test
+    void findAll() {
+        // (1)
+        String expectedName = "Fire Water";
+        String expectedBrand = "FIRE";
+        String expectedRegion = "Philadelphia";
+        int expectedSize = 750;
+        int expectedQty = 10;
+        double expectedPrice = 80.00;
+
+        // (2)
+        VodkaService VodkaService = new VodkaService();
+        Vodka testVodka = VodkaService.create(expectedName, expectedBrand,
+                expectedRegion, expectedSize, expectedQty, expectedPrice);
+        Vodka testVodka2 = VodkaService.create(expectedName, expectedBrand,
+                expectedRegion, expectedSize, expectedQty, expectedPrice);
+        Vodka testVodka3 = VodkaService.create(expectedName, expectedBrand,
+                expectedRegion, expectedSize, expectedQty, expectedPrice);
+
+
+        // (3)
+        Vodka actual= VodkaService.findVodka(testVodka.getId());
+        Vodka[] actual1 = VodkaService.findAll();
+        Vodka[] expected = {testVodka, testVodka2, testVodka3};
+
+        Assertions.assertArrayEquals(actual1, expected);
+
+
+    }
+
+    @Test
+    void delete() {
+        String expectedName = "Fire Water";
+        String expectedBrand = "FIRE";
+        String expectedRegion = "Philadelphia";
+        int expectedSize = 750;
+        int expectedQty = 10;
+        double expectedPrice = 80.00;
+
+        // (2)
+        VodkaService VodkaService = new VodkaService();
+
+        Vodka testVodka3 = VodkaService.create(expectedName, expectedBrand,
+                expectedRegion, expectedSize, expectedQty, expectedPrice);
+
+        int actualId = testVodka3.getId();
+        Vodka actual= VodkaService.findVodka(testVodka3.getId());
+
+
+
+        // (3)
+
+        Assertions.assertTrue(VodkaService.delete(actualId));
+
+    }
 }
